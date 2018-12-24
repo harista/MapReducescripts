5bcce132c22829fca228a003900b0d102fe21225
Haris <harista.90@gmail.com>
2018-12-24 21:14:03 +0400

    Update RatingsBreakdown.py

--- RatingsBreakdown.py
+++ RatingsBreakdown.py
@@ -1,18 +1,18 @@
-from mrjob.job import MRJob # Way to write MR jobs in python
+from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 class RatingsBreakdown(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_ratings,
-                reducer=self.reducer_count_ratigns)
+                   reducer=self.reducer_count_ratings)
         ]
 
     def mapper_get_ratings(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
         yield rating, 1
 
-    def reduce_count_ratings(self, key, values):
+    def reducer_count_ratings(self, key, values):
         yield key, sum(values)
 
 if __name__ == '__main__':
